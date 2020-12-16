a3d0e71c20fed415cedb07d0feb549cb9444f20d
aiventures <49552433+aiventures@users.noreply.github.com>
2020-12-16 15:32:03 +0100

    put logic into img_meta API

---
+++ copy_meta_files.py
@@ -0,0 +1,15 @@
+# util program to copy files from a source director to child directories
+from image_meta.persistence import Persistence
+
+#import atexit
+#atexit.register(input, 'Press Enter to continue...')
+fp = r"C:\<PATH TO YOUR ROOT>\PHOTOS"
+fp_src = r"C:\<PATH TO YOUR FOLDER FCONTAINING METADATA>\_META_"
+METADATA = "metadata.tpl"
+files_copy = ["metadata_exif.tpl",METADATA]
+# simulate / copy / display output
+save = False
+showinfo = True
+
+finished = Persistence.copy_meta_files(fp=fp,fp_src=fp_src,metadata=METADATA,files_copy=files_copy,save=save,showinfo=showinfo)
+input(f"Processing Finished; {finished}, hit key to exit")
