8ca1476ce93ac9b0c0a3cb9aafa0ddecaf48f566
hfessler <49552433+aiventures@users.noreply.github.com>
2023-01-22 17:16:42 +0100

    Corrections

--- img_waypt.py
+++ img_waypt.py
@@ -35,7 +35,7 @@ TIMESTAMP="TIMESTAMP"
 TIMEZONE="TIMEZONE"
 URL_OSM=r"https://www.openstreetmap.org/#map=19/LAT/LON"
 
-def read_image_meta(f_img:str,attributes="DateTimeOriginal"):
+def read_image_meta(f_img:str,attributes="DateTimeOriginal",exiftool="exiftool.exe"):
     """
       f: filename of image
       attributes: Exif Metadata atttibutes Use "*" to read all metadata
@@ -51,7 +51,7 @@ def read_image_meta(f_img:str,attributes="DateTimeOriginal"):
         print(f"Attributes {attributes} is not string or list")
         return {}
     att_list=" ".join([("-"+att) for att in att_list])
-    exiftool_cmd=EXIFTOOL_CMD.replace("EXIFTOOL","exiftool.exe")
+    exiftool_cmd=EXIFTOOL_CMD.replace("EXIFTOOL",exiftool)
     exiftool_cmd=exiftool_cmd.replace("FILE",f_img)
     exiftool_cmd=exiftool_cmd.replace("ATT",att_list)
     stream=os.popen(exiftool_cmd)
@@ -79,13 +79,13 @@ def convert_datetime_string(dts:str,tz:str="Europe/Berlin"):
         print(e)
     return out
 
-def get_replace_dict(f_img:str,f_gpx:str,time_gps:str=None,tz:str="Europe/Berlin"):
+def get_replace_dict(f_img:str,f_gpx:str,time_gps:str=None,tz:str="Europe/Berlin",exiftool="exiftool.exe"):
     """ based from timestamps and gps file, get relacement variables for waypoint template """
     DATE_TIME_ORIGINAL="DateTimeOriginal"
     REGEX_DATETIME=r"(\d{4}:\d{2}:\d{2} \d{2}:\d{2}:\d{2})"
 
     # get date time string from image
-    exif_json=read_image_meta(f_img,attributes=DATE_TIME_ORIGINAL)
+    exif_json=read_image_meta(f_img,attributes=DATE_TIME_ORIGINAL,exiftool=exiftool)
     date_time=exif_json.get(DATE_TIME_ORIGINAL)
     if date_time:
         dt_camera_s=re.findall(REGEX_DATETIME,date_time)
@@ -128,9 +128,9 @@ def get_replace_dict(f_img:str,f_gpx:str,time_gps:str=None,tz:str="Europe/Berlin
     replace_dict = {}
     if wpt_min:
         dt_wpt_s=DateTime.fromtimestamp(ts_min).strftime("%Y:%m:%d %H:%M:%S")
-        dt_now_s=DateTime.now().strftime("%Y:%m:%d %H:%M:%S")
+        dt_now_s=DateTime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
         datetime_wpt=convert_datetime_string(dt_wpt_s,tz)
-        dt_wpt_utc_s=datetime_wpt["UTC"].strftime("%Y:%m:%dT%H:%M:%SZ")
+        dt_wpt_utc_s=datetime_wpt["UTC"].strftime("%Y-%m-%dT%H:%M:%SZ")
         print(f"TRACK-WAYPOINT DIFFERENCE: {datetime_wpt[TIMESTAMP]-ts_gps}s")
         print(f"\n--- TRACK ({wpt_min['track_name']}) / ({dt_now_s}) ---")
         lat=str(round(wpt_min["lat"],6))
@@ -141,6 +141,8 @@ def get_replace_dict(f_img:str,f_gpx:str,time_gps:str=None,tz:str="Europe/Berlin
         replace_dict["_LAT_"]=lat
         replace_dict["_LON_"]=lon
         replace_dict["_ELE_"]=ele
+        wpt_name=DateTime.fromtimestamp(ts_min).strftime("%Y%m%d_%H%M%S")+"_CALIB"
+        replace_dict["_CALIB_"]=wpt_name
         osm_url=URL_OSM.replace("LAT",lat).replace("LON",lon)
         replace_dict["URL"]=osm_url
         print(f"    LATLON: [{lat}, {lon}], ELEVATION: {ele}m")
@@ -153,7 +155,8 @@ def get_replace_dict(f_img:str,f_gpx:str,time_gps:str=None,tz:str="Europe/Berlin
 
 def create_wpt_file(f_img:str,f_gpx:str,f_waypt_template:str,
                     time_gps:str=None,tz:str="Europe/Berlin",
-                    f_wpt:str="gps_wpt.gpx"):
+                    f_wpt:str="gps_wpt.gpx",
+                    exiftool:str="exiftool.exe"):
     """ creates a new waypoint file in image path """
 
     REGEX_TIME=r"^\d{2}:\d{2}:\d{2}"
@@ -176,8 +179,8 @@ def create_wpt_file(f_img:str,f_gpx:str,f_waypt_template:str,
     f_waypt=p_img.parent.joinpath(f_wpt)
     files["WPT"]=f_waypt
     for k,v in files.items():
-        print(f"{k:<15}: {v}")
-    replace_dict=get_replace_dict(f_img,f_gpx,time_gps,tz)
+        print(f"{k:<15}: {str(v)}")
+    replace_dict=get_replace_dict(f_img,f_gpx,time_gps,tz,exiftool)
 
     out=""
     lines=Persistence.read_file(f_waypt_template)
