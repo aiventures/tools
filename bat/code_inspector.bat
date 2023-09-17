fe85da60942b3843f8d4b651767fdc30fe9c275c
hfessler <49552433+aiventures@users.noreply.github.com>
2023-09-17 18:54:56 +0200

    bat tools add help file

--- bat/code_inspector.bat
+++ bat/code_inspector.bat
@@ -4,7 +4,6 @@ call set_env_local.bat
 call venv_work.bat
 echo #### RUN Code Inspector (%code_inspector%)
 setlocal
-rem if no file is given, transform all
 if "%~1"=="" (set workdir=%CD%) else (set workdir=%1)
 echo %code_inspector% %workdir%
 python %code_inspector% %workdir%
---
+++ bat/h.bat
@@ -0,0 +1,10 @@
+@echo off
+rem help file
+echo ### HELP
+echo * code_inspector [path]    create plantuml for current dir, or [path]
+echo * plantuml [file]          create plantuml from [file], or from all plantuml in current dir
+echo * set_env_local,           setting local variables (use set_env_template as template)
+echo                            code_inspector, plantuml, tc, venv_work 
+echo * tc [right_folder]        opens total commander at current location 
+echo                            optionally [right_folder] for right view
+echo * venv_work                activates VENV [venv_work}
